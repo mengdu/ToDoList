ee4fea2f524c06db0120e6bcb27461687617baa6
lanyue <lanyueos@qq.com>
2020-03-15 00:16:14 +0800

    add: 添加页脚

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import ToDo from './components/ToDo'
+import { LocalStore } from './components/ToDo/store'
+import WebsiteFooter from './components/website-footer'
+
+const store = new LocalStore()
 
 function App() {
   return (
-    <div className="App">
+    <div className="App main-container">
       <div className="todo-box">
         <h1 className="app-title">Task ToDo</h1>
-        <ToDo />
+        <ToDo store={store}/>
       </div>
+
+      <WebsiteFooter />
     </div>
   )
 }
---
+++ src/components/website-footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function WebsiteFooter () {
+  return (
+    <div className="website-footer">
+      <p className="copyright">
+        <a href="https://github.com/mengdu" target="_blank" rel="noopener noreferrer">Lanyue</a>&nbsp;
+        版权所有 &copy; 2020</p>
+    </div>
+  )
+}
