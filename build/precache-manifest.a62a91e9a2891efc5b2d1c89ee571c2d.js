458973c46e3643f3dd708019c63e5931e63a2328
lanyue <lanyueos@qq.com>
2020-03-18 21:10:41 +0800

    build

---
+++ build/precache-manifest.a62a91e9a2891efc5b2d1c89ee571c2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2479776889adf9ebba19f40b591ffc23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aafd9d07e021fa5ea1b6",
+    "url": "/static/css/main.6d0e5d3d.chunk.css"
+  },
+  {
+    "revision": "62b0dfc47a539babd405",
+    "url": "/static/js/2.d0266585.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.d0266585.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aafd9d07e021fa5ea1b6",
+    "url": "/static/js/main.c126a0b9.chunk.js"
+  },
+  {
+    "revision": "358b3f030b0ae0ca827c",
+    "url": "/static/js/runtime-main.ff225a3e.js"
+  },
+  {
+    "revision": "ed11d7bc9ed4efa19d6dc5b3c32f7184",
+    "url": "/static/media/task_done.ed11d7bc.mp3"
+  }
+]);
